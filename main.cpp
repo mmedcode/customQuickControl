3a9861914d3080496b402849da6bd9f40dbd531b
mmedcode <mmansourienit@gmail.com>
2025-02-18 21:44:17 +0100

    initial commit: example of quick control

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+
+int main(int argc, char *argv[])
+{
+    QGuiApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+
+    const QUrl style(QStringLiteral("qrc:/Style.qml"));
+    qmlRegisterSingletonType(style, "Style", 1, 0, "Style");
+
+
+    QObject::connect(
+        &engine,
+        &QQmlApplicationEngine::objectCreationFailed,
+        &app,
+        []() { QCoreApplication::exit(-1); },
+        Qt::QueuedConnection);
+    engine.loadFromModule("customQuickControl", "Main");
+
+    return app.exec();
+}
